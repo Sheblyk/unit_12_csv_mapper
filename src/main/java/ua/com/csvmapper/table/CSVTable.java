ef3901fbb0c30bab0df495e54a2c21146ba3b37e
Sheblyk <kokosigel@gmail.com>
2021-07-23 18:24:31 +0300

    fixed structure TableCSV (changed array to map in order to decrease time complexity)

--- src/main/java/ua/com/csvmapper/table/CSVTable.java
+++ src/main/java/ua/com/csvmapper/table/CSVTable.java
@@ -4,15 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class CSVTable {
     private static final String SPLIT = ",";
-    private String[] headers;
+    private LinkedHashMap<String, Integer> headers;
     private List<String[]> values;
 
-    public List<String[]> getValues(){
+    public List<String[]> getValues() {
         return values;
     }
 
@@ -20,7 +21,11 @@ public class CSVTable {
         try (BufferedReader reader = new BufferedReader(new FileReader(URI))) {
             String header;
             if ((header = reader.readLine()) != null) {
-                headers = header.split(SPLIT);
+                String[] headersArray = header.split(SPLIT);
+                headers = new LinkedHashMap<>();
+                for (int i = 0; i < headersArray.length; i++) {
+                    headers.put(headersArray[i], i);
+                }
                 values = new ArrayList<>();
                 String line;
                 while ((line = reader.readLine()) != null) {
@@ -33,31 +38,29 @@ public class CSVTable {
     }
 
     public String getElementByIndexes(int row, int col) {
-        if (row-1 < values.size() && col-1 < headers.length) {
+        if (row - 1 < values.size() && col - 1 < headers.size()) {
             return values.get(row - 1)[col - 1];
         }
         throw new IllegalArgumentException("Row index more than " + values.size() + 1 +
-                " or row index more than " + headers.length + 1);
+                " or row index more than " + headers.size() + 1);
     }
 
     public String getElementByName(int row, String colName) {
-        int col = getIndexByName(colName);
-        if (row -1 < values.size()) {
-            return values.get(row-1)[col];
+        Integer col = getIndexByName(colName);
+        if(col == null){
+            throw new NoSuchElementException("Column " + colName + " doesn`t exist");
+        }
+        else if (row - 1 < values.size() & col < headers.size()) {
+            return values.get(row - 1)[col];
         }
         throw new IllegalArgumentException("Row index more than " + values.size() + 1);
     }
 
-    private int getIndexByName(String colName) {
-        for (int i = 0; i < headers.length; i++) {
-            if (headers[i].equals(colName)) {
-                return i;
-            }
-        }
-        throw new IllegalArgumentException("Doesn`t exist column " + colName);
+    private Integer getIndexByName(String colName) {
+        return headers.get(colName);
     }
 
     public List<String> getHeaders() {
-        return Arrays.asList(headers);
+        return new ArrayList<>(headers.keySet());
     }
 }
